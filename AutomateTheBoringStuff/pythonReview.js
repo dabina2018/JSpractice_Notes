c273940d0904c20a008643161d5ab7f15349760f
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2022-06-20 11:59:05 -0400

    continueing JS practice

---
+++ AutomateTheBoringStuff/pythonReview.js
@@ -0,0 +1,10 @@
+
+//Example
+var firstName = "Ada"
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+//Setup
+var lastName = "Lovelace";
+
+//OFind last letter of last name
+var lastLetterOfLastName = lastName[length];
